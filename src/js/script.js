516c8c7af21b9cc8dce80b0b55c77d36c4bb6f71
Mila Urs <ursadev@gmail.com>
2021-06-03 18:13:37 +0300

    add slider

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+/* let answer = confirm("Do you have 18 years old?");
+console.log(answer); */
+
+
+
+/* if (2*4 == 8*2) {
+    console.log('Yes')
+} else {
+    console.log('No');
+} */
+
+
+
+/* let answer = prompt("Are you Mila?")
+if (answer =='yes') {
+    console.log('Welcome');
+} else {
+    console.log('Go away!');
+} */
+
+
+/* 
+const num = 52;
+if (num < 49) {
+    console.log("Wrong");
+} else if (num > 51) {
+    console.log("Wrong, sorry");
+} else {
+    console.log('Coool, you win!');
+} */
+
+
+
+// ЦИКЛЫ->
+
+/* for (let i = 1; i < 8; i++) {
+    console.log(i);
+} */
+
+
+//ФУНКЦИИ 
+
+/* function funcName(a, b) {
+    console.log(a+b)
+}
+funcName(3, 5); */
+
+
+
+$(document).ready(function(){
+    $('.testimonials__carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+  });
+          
