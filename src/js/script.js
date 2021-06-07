4a7c35bf462590459d4fe2b78451a301b65cd3f1
Mila Urs <ursadev@gmail.com>
2021-06-08 00:37:29 +0300

    add tabs

--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,29 @@ funcName(3, 5); */
         dotsClass: 'slick-dots',
 
     });
+
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.catalog__wrapper').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+
+
+      function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog__item-content').eq(i).toggleClass('catalog__item-content_active');
+                $('.catalog__item-about').eq(i).toggleClass('catalog__item-about_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog__item-link');
+    toggleSlide('.catalog__item-about-back');
+      
   });
 
 
